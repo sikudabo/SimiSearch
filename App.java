9766ab58a7479090335c273872bf80e8cf47193e
Simeon Ikudabo <32518831+sikudabo@users.noreply.github.com>
2018-08-30 13:45:09 -0400

    Main Java Class
    The main Java class that calls the other classes in the package to store information to the MySQL database from the .JSON data.

---
+++ App.java
@@ -0,0 +1,14 @@
+package com.simi.MySQL.com.send.data.toMySQL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        HighSchoolData.getSchools(); 
+        HighSchoolData.hsToMySQL();
+    }
+}
